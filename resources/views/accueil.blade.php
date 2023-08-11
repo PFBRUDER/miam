5eea6eccd20f579ede4bd62196639dffc66537a8
BrendanB <bouedec97@gmail.com>
2023-08-11 10:16:04 +0200

    acceuil 3

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
+
 <body>
-  <p>Hellooooo, it' PF</p>
-  <p>ça marche?</p>
-  
+    <p>Hellooooo, it' PF</p>
+    <p>ça marche?</p>
+    <p>Oui</p>
 </body>
+
 </html>
