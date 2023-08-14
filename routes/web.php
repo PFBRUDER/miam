9df0e4226116fe687057d63caba9f99c92e3a505
PFBRUDER <pfbruder2@gmail.com>
2023-08-14 14:50:35 +0200

    receipes Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReceipesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/receipes', [ReceipesController::class, 'receipes_list']);
